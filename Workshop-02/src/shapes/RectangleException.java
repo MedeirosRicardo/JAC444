02383c03e1c63db06ea457e4a27d5860d323a06e
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-01-30 14:40:51 -0500

    created all exception class

---
+++ Workshop-02/src/shapes/RectangleException.java
@@ -0,0 +1,13 @@
+package shapes;
+
+/** This class handles exceptions for the Rectangle class */
+public class RectangleException extends Exception {
+
+	/**
+	 * Constructor accepts a string for the message
+	 * @param message A string representing the message
+	 */
+	public RectangleException(String message) {
+		super(message);
+	}
+}
--- Workshop-02/src/shapes/SquareException.java
+++ Workshop-02/src/shapes/SquareException.java
@@ -1,13 +1,13 @@
 package shapes;
 
-/** This class handles exceptions for the Circle class */
-public class CircleException extends Exception {
+/** This class handles exceptions for the square class */
+public class SquareException extends Exception {
 	
 	/**
 	 * Constructor accepts a string for the message
 	 * @param message A string representing the message
 	 */
-	public CircleException(String message) {
+	public SquareException(String message) {
 		super(message);
 	}
 }
