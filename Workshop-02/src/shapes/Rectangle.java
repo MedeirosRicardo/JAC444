79c55f30e25d321c776f0586733203b09abb867e
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-02-02 02:48:00 -0500

    created class parallelogram

--- Workshop-02/src/shapes/Rectangle.java
+++ Workshop-02/src/shapes/Rectangle.java
@@ -11,7 +11,7 @@ public class Rectangle implements Shape{
 	 * Constructor accepts two double for width and height
 	 * @param width A positive double representing the width of the rectangle
 	 * @param height A positive double representing the height of the rectangle
-	 * @throws RectangleException Throws a message if data is no valid
+	 * @throws RectangleException Throws a message if data is not valid
 	 */
 	public Rectangle(double width, double height) throws RectangleException {
 		if (width > 0 && height > 0) {
