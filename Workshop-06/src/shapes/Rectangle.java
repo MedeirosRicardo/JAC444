885cd086e2ba41b06777395b8d0f901ea1e3d27b
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-04 17:44:14 -0500

    Workshop 6 - done

--- Workshop-06/src/shapes/Rectangle.java
+++ Workshop-06/src/shapes/Rectangle.java
@@ -1,5 +1,7 @@
 package shapes;
 
+import java.text.DecimalFormat;
+
 /** This class creates a shape of type rectangle */
 public class Rectangle extends Parallelogram {
 		
@@ -12,5 +14,27 @@ public class Rectangle extends Parallelogram {
 	public Rectangle(double width, double height) throws ParallelogramException {
 		super(width, height);
 	}
+	
+	/**
+	 * Returning a string representing the rectangle
+	 * @return a string representing the rectangle
+	 */
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " {w=" + getWidth() + ", h=" + getHeight() 
+			+ "} perimeter = " + new DecimalFormat("0.00000").format(calculatePerimeter())
+			+ " area = " + new DecimalFormat("0.00000").format(calculateArea(getWidth(), area));
+	}
+	
+	// Workshop 6
+	/**
+	 * Calculate the area of the rectangle
+	 * @return a double indicating the area of the rectangle
+	 */
+	public double calculateArea(double area, ShapeArea s) {
+		return s.calculateArea(area);
+	}
+	
+	ShapeArea area = width-> getWidth() * getHeight();
 
 }
